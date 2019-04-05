3f901daa7dbb6412d23395132b55192193225baf
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-05 21:01:02 +0100

    Adds functionality to disallow for addition of more than two operands

--- add-two-numbers/addTwoNumbers.js
+++ add-two-numbers/addTwoNumbers.js
@@ -1,4 +1,5 @@
 function addTwoNumbers(a, b) {
+	if (arguments.length > 2) return 'Invalid input: Can only add two numbers';
 	return (Number(a) * 10 + Number(b) * 10) / 10;
 }
 module.exports = addTwoNumbers;
