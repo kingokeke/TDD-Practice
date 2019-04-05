a0daee2ececbd0aebc0213a41131c448f55f691c
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-05 21:14:49 +0100

    Adds functionality to disallow adding of less than 2 numbers

--- add-two-numbers/addTwoNumbers.js
+++ add-two-numbers/addTwoNumbers.js
@@ -1,5 +1,5 @@
 function addTwoNumbers(a, b) {
-	if (arguments.length > 2) return 'Invalid input: Can only add two numbers';
+	if (arguments.length !== 2) return 'Invalid input: You must provide two numbers only.';
 	return (Number(a) * 10 + Number(b) * 10) / 10;
 }
 module.exports = addTwoNumbers;
