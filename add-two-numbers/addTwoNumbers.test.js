3277ed855e56c75094605acb5fea23dbe7b48522
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-05 16:21:46 +0100

    Adds test to allow for addition of strings that can be coerced to numbers

--- add-two-numbers/addTwoNumbers.test.js
+++ add-two-numbers/addTwoNumbers.test.js
@@ -8,4 +8,8 @@ describe('Add two numbers', function() {
 		expect(addTwoNumbers(-3, 0)).toBe(-3);
 		expect(addTwoNumbers(-12, -9)).toBe(-21);
 	});
+
+	it('should allow for automatic type coercion of string numbers', function() {
+		expect(addTwoNumbers('2', '2')).toBe(4);
+	});
 });
