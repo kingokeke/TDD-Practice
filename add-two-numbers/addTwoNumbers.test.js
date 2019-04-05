1d816c92d5f23fa5c2025156b5605de5a7a2f0ad
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-05 16:20:14 +0100

    Adds more tests for adding two numbers

--- add-two-numbers/addTwoNumbers.test.js
+++ add-two-numbers/addTwoNumbers.test.js
@@ -5,5 +5,7 @@ var addTwoNumbers = require('./addTwoNumbers.js');
 describe('Add two numbers', function() {
 	it('should add two numbers', function() {
 		expect(addTwoNumbers(1, 1)).toBe(2);
+		expect(addTwoNumbers(-3, 0)).toBe(-3);
+		expect(addTwoNumbers(-12, -9)).toBe(-21);
 	});
 });
