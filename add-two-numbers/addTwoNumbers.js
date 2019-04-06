d9aab873a3f41d7fd8adc35a9190c1eb0df4408a
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-06 09:19:35 +0100

    Refactors function to handle parameter checks using a loop

--- add-two-numbers/addTwoNumbers.js
+++ add-two-numbers/addTwoNumbers.js
@@ -1,12 +1,10 @@
 function addTwoNumbers(a, b) {
 	if (arguments.length !== 2) return 'Invalid input: You must provide two numbers only.';
-	if (typeof a === 'object') return 'Invalid input: You must provide two numbers only.';
-	if (typeof a === 'boolean') return 'Invalid input: You must provide two numbers only.';
-	if (typeof b === 'object') return 'Invalid input: You must provide two numbers only.';
-	if (typeof b === 'boolean') return 'Invalid input: You must provide two numbers only.';
-	if (Number.isNaN(Number(a))) return 'Invalid input: You must provide two numbers only.';
-	if (Number.isNaN(Number(b))) return 'Invalid input: You must provide two numbers only.';
-
+	for (var i = 0; i < 2; i++) {
+		if (typeof arguments[i] === 'object' || typeof arguments[i] === 'boolean' || Number.isNaN(Number(arguments[i]))) {
+			return 'Invalid input: You must provide two numbers only.';
+		}
+	}
 	return (Number(a) * 10 + Number(b) * 10) / 10;
 }
 module.exports = addTwoNumbers;
