29ba3e1baa5cb60a4a2e29c81155a950b1f955b0
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-06 09:09:46 +0100

    Adds functionality to disallow addition for data types that are not numbers or strings that coerce to numbers

--- add-two-numbers/addTwoNumbers.js
+++ add-two-numbers/addTwoNumbers.js
@@ -1,5 +1,12 @@
 function addTwoNumbers(a, b) {
 	if (arguments.length !== 2) return 'Invalid input: You must provide two numbers only.';
+	if (typeof a === 'object') return 'Invalid input: You must provide two numbers only.';
+	if (typeof a === 'boolean') return 'Invalid input: You must provide two numbers only.';
+	if (typeof b === 'object') return 'Invalid input: You must provide two numbers only.';
+	if (typeof b === 'boolean') return 'Invalid input: You must provide two numbers only.';
+	if (Number.isNaN(Number(a))) return 'Invalid input: You must provide two numbers only.';
+	if (Number.isNaN(Number(b))) return 'Invalid input: You must provide two numbers only.';
+
 	return (Number(a) * 10 + Number(b) * 10) / 10;
 }
 module.exports = addTwoNumbers;
