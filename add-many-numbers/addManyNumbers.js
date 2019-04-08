da365dac6698b57508bc2984e64d66992e718d82
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-08 10:55:49 +0100

    Adds functionality to disallow addition of strings that don't coerce to numbers

--- add-many-numbers/addManyNumbers.js
+++ add-many-numbers/addManyNumbers.js
@@ -1,6 +1,9 @@
 function addManyNumbers() {
 	var sum = 0;
 	for (var i = 0; i < arguments.length; i++) {
+		if (typeof arguments[i] === 'string' && arguments[i] !== '0' && !Number(arguments[i])) {
+			return 'Invalid input. Please enter only numbers';
+		}
 		sum = (sum * 10 + arguments[i] * 10) / 10;
 	}
 	return sum;
