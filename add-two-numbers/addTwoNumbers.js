eee09ae679e95ccc983ef2f9ce6e2f5295a160b5
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-08 08:45:16 +0100

    Refactors the code to make it more robust

--- add-two-numbers/addTwoNumbers.js
+++ add-two-numbers/addTwoNumbers.js
@@ -1,7 +1,11 @@
 function addTwoNumbers(a, b) {
 	if (arguments.length !== 2) return 'Invalid input: You must provide two numbers only.';
 	for (var i = 0; i < 2; i++) {
-		if (typeof arguments[i] === 'object' || typeof arguments[i] === 'boolean' || Number.isNaN(Number(arguments[i]))) {
+		if (
+			typeof arguments[i] === 'object' ||
+			typeof arguments[i] === 'boolean' ||
+			(arguments[i] !== 0 && arguments[i] !== '0' && !Number(arguments[i]))
+		) {
 			return 'Invalid input: You must provide two numbers only.';
 		}
 	}
