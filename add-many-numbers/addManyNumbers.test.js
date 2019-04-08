5594f0dbd0a88129baf3cfab5c6f208d10314d2d
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-08 10:21:57 +0100

    Adds test to check for 0.1 + 0.2 float addition anomaly

--- add-many-numbers/addManyNumbers.test.js
+++ add-many-numbers/addManyNumbers.test.js
@@ -1,6 +1,14 @@
+/* eslint-disable no-undef */
+
 var addManyNumbers = require('./addManyNumbers.js');
+
 describe('Add many numbers', function() {
 	it('should add multiple numbers', function() {
 		expect(addManyNumbers(2, 3, 4, 5, 6, 7, 8, 9)).toBe(44);
+	});
+
+	it('should add multiple float numbers', function() {
+		expect(addManyNumbers(2.7, 9.4, 4.1, 1.9, 3.7, 5.8, 0.0001, 7.5)).toBe(35.1001);
+		expect(addManyNumbers(0.1, 0.1, 0.1)).toBe(0.3);
 	});
 });
