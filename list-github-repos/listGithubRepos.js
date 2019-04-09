809787babdd6268e761a9c58feebe46892ad6f64
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-10 00:24:44 +0100

    Adds functionality to disallow non-string parameters

--- list-github-repos/listGithubRepos.js
+++ list-github-repos/listGithubRepos.js
@@ -1,5 +1,6 @@
 function listGithubRepos(username) {
 	var axios = require('axios');
+	if (typeof username !== 'string') return 'Error';
 	return axios.get('http://localhost:3000/users?username=' + username);
 }
 module.exports = listGithubRepos;
