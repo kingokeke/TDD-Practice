ecec471bbad9b0a9e7795de081365da00b602d99
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-10 00:07:42 +0100

    Refactors the function to remove excess code

--- list-github-repos/listGithubRepos.js
+++ list-github-repos/listGithubRepos.js
@@ -1,7 +1,5 @@
 function listGithubRepos(username) {
 	var axios = require('axios');
-	var url = 'http://localhost:3000/users?username=' + username;
-	return axios.get(url);
+	return axios.get('http://localhost:3000/users?username=' + username);
 }
-listGithubRepos('kingokeke');
 module.exports = listGithubRepos;
