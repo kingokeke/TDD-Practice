f3599423d22c4beaea0858afadd80f36b7789c15
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-10 10:10:14 +0100

    Adds test to allow only strings as parameters

--- concatenate-two-strings/concatenate.test.js
+++ concatenate-two-strings/concatenate.test.js
@@ -4,4 +4,7 @@ describe('Concatenate two strings', function() {
 	it('should accept only two parameters', function() {
 		expect(concatenate('iron', 'man')).toEqual('ironman');
 	});
+	it('should accept only strings', function() {
+		expect(concatenate('oceans', 11)).toBe('Error. Invalid input');
+	});
 });
