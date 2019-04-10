ab25f8d069cfd86fe5154c84a6f5bbf0fcbe10ca
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-10 10:23:37 +0100

    Adds more tests for accepting only strings as parameters

--- concatenate-two-strings/concatenate.test.js
+++ concatenate-two-strings/concatenate.test.js
@@ -9,5 +9,10 @@ describe('Concatenate two strings', function() {
 	});
 	it('should accept only strings', function() {
 		expect(concatenate('oceans', 11)).toBe('Error. Invalid input');
+		expect(concatenate('oceans', {suffix: 'eight'})).toBe('Error. Invalid input');
+		expect(concatenate('oceans', ['eight', 'eleven'])).toBe('Error. Invalid input');
+		expect(concatenate('oceans', null)).toBe('Error. Invalid input');
+		expect(concatenate('oceans', undefined)).toBe('Error. Invalid input');
+		expect(concatenate('oceans', false)).toBe('Error. Invalid input');
 	});
 });
