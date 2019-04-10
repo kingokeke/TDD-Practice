287749b78d56bb8cd8fcd4e3d2e3e3681203da23
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-10 10:17:08 +0100

    Adds more tests to allow only two parameters

--- concatenate-two-strings/concatenate.test.js
+++ concatenate-two-strings/concatenate.test.js
@@ -3,6 +3,9 @@ var concatenate = require('./concatenate.js');
 describe('Concatenate two strings', function() {
 	it('should accept only two parameters', function() {
 		expect(concatenate('iron', 'man')).toEqual('ironman');
+		expect(concatenate('iron', 'man', 'acles')).toEqual('Error. Invalid input');
+		expect(concatenate('iron')).toEqual('Error. Invalid input');
+		expect(concatenate()).toEqual('Error. Invalid input');
 	});
 	it('should accept only strings', function() {
 		expect(concatenate('oceans', 11)).toBe('Error. Invalid input');
